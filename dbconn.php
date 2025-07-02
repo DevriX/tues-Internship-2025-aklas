3d1b0691e7a6fb8f69b7668804212fac0d0a31f5
Christian Nikolov <kristian.b.nikolov.2021@elsys-bg.org>
2025-07-02 13:55:47 +0300

    db connection established

---
+++ dbconn.php
@@ -0,0 +1,15 @@
+<?php
+    define("HOSTNAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "job_board");
+
+    $connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+    
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    else{
+        echo "Connected successfully to database 'job_board'";
+    }
+?>
