bb684aa3079d50a335fd1837aa0e7360c15cf3d7
Christian Nikolov <kristian.b.nikolov.2021@elsys-bg.org>
2025-07-08 16:37:54 +0300

    Created a config.php which conatins the main path to the project, so we can just rename it there and not everywhere

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+$project_path = 'tues-Internship-2025-aklas';
+define('PROJECT_PATH', $project_path);
--- header.php
+++ header.php
@@ -1,12 +1,13 @@
 <?php
+require_once 'config.php';
 // Shared header partial. Assumes $user_logged_in, $display_name, $current_page are set in the including file.
 ?>
 <header class="site-header main-header-navbar">
     <div class="row site-header-inner">
         <div class="site-header-branding">
-            <h1 class="site-title"><a href="/tues-Internship-2025-aklas/index.php">Job Offers</a></h1>
+            <h1 class="site-title"><a href="/<?= $project_path ?>/index.php">Job Offers</a></h1>
         </div>
-        <nav class="site-header-navigation">
+        <nav class="site-header-navigation">    
             <ul class="menu">
                 <?php if (isset($user_logged_in) && $user_logged_in): ?>
                     <li class="menu-item">
@@ -20,10 +21,10 @@
                     </li>
                 <?php else: ?>
                     <li class="menu-item">
-                        <a href="/tues-Internship-2025-aklas/register.php">Register</a>
+                        <a href="/<?= $project_path ?>/register.php">Register</a>
                     </li>
                     <li class="menu-item">
-                        <a href="/tues-Internship-2025-aklas/login.php">Login</a>
+                        <a href="/<?= $project_path ?>/login.php">Login</a>
                     </li>
                 <?php endif; ?>
             </ul>
